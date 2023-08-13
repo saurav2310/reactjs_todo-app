1b3f95c8adaf20bb6e372adc6ace01026de5be26
Saurav <saurav@pal.com>
2023-08-13 17:50:29 +0530

    fixed Home path

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 import Header from "./components/Header";
 import Profile from "./pages/Profile";
 import Login from "./pages/Login";
